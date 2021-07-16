583a06596e78c0363ab680764b2b76d1174f69ad
Sundar <sonu143nagarkoti@gmail.com>
2021-07-16 16:42:17 +0530

    u

--- movies.js
+++ movies.js
@@ -63,3 +63,5 @@
       dailyRentalRate: 3.5
     }
   ];
+
+ 
--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 let express=require("express");
 
 let server=express(); //this line makes server
+
+let data=require("./data.json");
+
+let g=data.map((e)=>{
+    return e.genre;
+})
+
+
+server.get("/movies",function(req,res){
+    res.send(data);
+});
+
+server.get("/genre",function(req,res){
+    res.send(g);
+});
+
+server.listen(4000);
